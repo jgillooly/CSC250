3a0bae5df6bc9fea3615244d7015b0c7a0243d4c
Joshua Gillooly <jgillooly@student.neumont.edu>
2025-12-08 23:02:47 -0700

    Implemented a sorting facade to augment the previously implemented strategies and automatically select a sort algorithm based on the length of the array to be sroted

---
+++ CSC250/Sorts/SortFacade.cs
@@ -0,0 +1,21 @@
+﻿using CSC250.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC250.Sorts
+{
+    internal class SortFacade
+    {
+        public static int[] sortInts(int[] ints)
+        {
+            Context context = new Context();
+            ISortingStrategy strategy = ints.Length <= 1000 ? new BubbleSort() : new InsertionSort();
+            context.setStrategy(strategy);
+            ints = context.sortInts(ints);
+            return ints;
+        }
+    }
+}
