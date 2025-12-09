b34d655dce58235a853fc84d940fb1e8324ddf11
Joshua Gillooly <jgillooly@student.neumont.edu>
2025-12-08 23:17:18 -0700

    Implemented a proxy for the file read system that caches the word lists it has already read and returns the cache if the file has not been modified since it was last read

---
+++ CSC250/WordFileProxy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC250
+{
+    internal class WordFileProxy
+    {
+        private static WordFileProxy? instance = null;
+        private Dictionary<string, List<string>>? wordCache = new Dictionary<string, List<string>>();
+        private Dictionary<string, DateTime>? lastUpdated = new Dictionary<string, DateTime>();
+        private WordFileProxy()
+        {
+        }
+
+        public static WordFileProxy getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new WordFileProxy();
+            }
+            return instance;
+        }
+
+        private List<string> GetWordsFromFile(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            Array.Sort(lines);
+            var list = lines.ToList();
+            return list;
+        }
+
+        public List<string> GetWordsByPath(string path)
+        {
+            if (wordCache[path] != null && File.GetLastWriteTime(path) == lastUpdated[path])
+            {
+                return wordCache[path];
+            }
+            else
+            {
+                var words = GetWordsFromFile(path);
+                wordCache[path] = words;
+                lastUpdated[path] = File.GetLastWriteTime(path);
+                return words;
+            }
+        }
+    }
+}
